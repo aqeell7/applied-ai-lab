7792cc2049091425d035bc21eecb3c0bf002850e
Aqeel Ahmed <md.aqeelahmed007@gmail.com>
2026-08-19 00:31:49 +0530

    feat: streaming chat CLI via openai SDK.

---
+++ src/d-164/openai-sdk.ts
@@ -0,0 +1,50 @@
+import OpenAI from "openai";
+import "dotenv/config"
+
+const question = process.argv.slice(2).join(" ")
+
+const openai = new OpenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+    baseURL:"https://generativelanguage.googleapis.com/v1beta/openai/"
+})
+
+async function main(){
+
+if(!question){
+      return console.log("you forgot to enter your question, please enter your question after npx tsx src/index.ts")
+    }
+
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY; 
+    if(!GEMINI_API_KEY){
+        return console.log("there is not api key or unable to fetch key")
+    } 
+
+    try{
+     const completions = await openai.chat.completions.create({
+        model: "gemini-2.5-flash",
+        messages :[{
+            role: "system",
+            content: "you are a helpful assistant"
+        },
+        {
+            role:"user",
+            content: question
+        }
+    ],
+    stream: true
+    })
+
+    for await(const chunk of completions){
+        const content = chunk.choices[0]?.delta?.content
+
+        if(content){
+        process.stdout.write(content)
+    }
+    }
+
+    }catch(error){
+        console.log(error)
+    }
+}
+
+main();
